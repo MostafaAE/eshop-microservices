51fb9e9305832f567c8728f9241101a575ea6325
MostafaAE <mostafa.aymanae@gmail.com>
2024-10-11 21:31:01 +0300

    Refactor namespaces

--- src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -1,16 +1,15 @@
-﻿namespace BuildingBlocks.Exceptions
+﻿namespace BuildingBlocks.Exceptions;
+
+public class BadRequestException : Exception
 {
-    public class BadRequestException : Exception
+    public string? Details { get; }
+
+    public BadRequestException(string message) : base(message)
+    { }
+
+    public BadRequestException(string message, string details)
+        : base(message)
     {
-        public string? Details { get; }
-
-        public BadRequestException(string message) : base(message)
-        { }
-
-        public BadRequestException(string message, string details)
-            : base(message)
-        {
-            Details = details;
-        }
+        Details = details;
     }
 }
