a1d64b87f88b02150f597b8918d6d9655201e1b0
MostafaAE <mostafa.aymanae@gmail.com>
2024-10-26 20:30:20 +0300

    Implement delete coupon functionality

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -52,4 +52,20 @@ public class DiscountService(DiscountContext dbContext, ILogger<DiscountService>
         var couponModel = coupon.Adapt<CouponModel>();
         return couponModel;
     }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons
+            .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName= {request.ProductName} is not found."));
+
+        dbContext.Remove(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount is successfully deleted for ProductName: {productName}", coupon.ProductName);
+
+        return new DeleteDiscountResponse { Success = true};
+    }
 }
