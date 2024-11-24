089c04daaf492a8720b62a9fcc6e7c7e0e2eab25
MostafaAE <mostafa.aymanae@gmail.com>
2024-11-24 23:31:34 +0200

    Implement get paginated orders handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,26 @@
+﻿
+using BuildingBlocks.Pagination;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+public class GetOrdersHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+{
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        int pageIndex = query.PaginationRequest.pageIndex;
+        int pageSize = query.PaginationRequest.pageSize;
+        long count = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .OrderBy(o => o.OrderName.Value)
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        var paginatedResult = new PaginatedResult<OrderDto>(pageIndex, pageSize, count, orders.ToOrderDtoList());
+        return new GetOrdersResult(paginatedResult);
+    }
+}
